bb4cade8bddf4277fc836138fc30ef4a5ca6c0dc
Dewni-hasa <87058837+Dewni-hasa@users.noreply.github.com>
2021-07-11 21:46:48 +0530

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,47 +1,42 @@
-public class Player {
-    private String Name;
-    private int Age;
-    private String Type;
-    private int[] Statistics=new int[15];
+public class Player{
 
-    public Player(String N , int A , String T , int[] Stat){
-        Name=N;
-        Age=A;
-        Type=T;
-        Statistics=Stat;
-    }
+	String Name;
+	int Age;
+	String Type;
+	int[] Statistics=new int[15];
 
-    public double getAverage(){
-        double total=0;
-        for(int i=0;i<15;i++){           //Batting Average for last 15 matches....gives a single match batting Average
-            total=total+Statistics[i];  //Bowling Average for last 15 matches.... gives a single match wicket taking Average.             //Wicket Keeping stemping Average for last 15 matches...
-        }
-        return(total/15);
-    }
+	public Player(String N, int A, String T, int[]Stat){
+ 
+		Name = N;
+		Age = A;
+		Type = T;
+		Statistics = Stat;
+	}
 
-    public void getPlayerDetails(){
-        System.out.println("Player name : " +Name);
-        System.out.println("Player age : " +Age);
-        System.out.println("Player type : " +Type);
+	public void getAverage(){
+		double total = 0;
+		for(int i=0;i<15;i++)
+		{
+			total = total+Statistics[i];
+		}
+	System.out.println(total/15);
 
-    }
+	}
 
-    public int getBestPerformance(){
-        int highest=0;                       
-        for(int i = 0;i<15;i++){
-            if (Statistics[i]>highest){
-                highest=Statistics[i];
-            }
-        }
-        return highest;
-    }
+	public void getPlayerDetails(){
+		System.out.println("Player name : "+Name);
+		System.out.println("Player age : "+Age);
+		System.out.println("Player type : "+Type);
+	}
 
-    public String getType(){
-        return Type;
-    }
+	public void getBestPerformance(){
+		int highest = 0;
+		for(int i = 0;i<15;i++){
+			if(Statistics[i] > highest){
+				highest = Statistics[i];
+			}
+		}
+	System.out.println("Highest " +highest);
 
-    public String getName(){
-        return Name;
-    }
-
+	}
 }
